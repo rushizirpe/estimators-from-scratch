6fc8c3b165db5398f02f7a0f7962b578a10de450
Rishi <62651883+rushizirpe@users.noreply.github.com>
2023-05-27 15:59:31 +0530

    Create k_means.py

---
+++ k_means.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class KMeans:
+    """K-Means Class"""
+    
+    def __init__(self, data, num_clusters):
+        pass
+
+    def train(self, max_iterations):
+        pass
+
+    def centroids_init(data, num_clusters):
+        pass
+
+    def centroids_find_closest(data, centroids):
+        pass
+
+    def centroids_compute(data, closest_centroids_ids, num_clusters):
+        pass
